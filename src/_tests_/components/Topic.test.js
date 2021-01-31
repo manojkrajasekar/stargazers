1cd4a1042501413f4d29ac9b86b4ebf0a3a5b1b5
manojkrajasekar <manojkrajasekar@gmail.com>
2021-01-31 12:18:26 -0600

    add additional test

--- src/_tests_/components/Topic.test.js
+++ src/_tests_/components/Topic.test.js
@@ -20,3 +20,30 @@ test("Renders Topic Appropriately", () => {
   const linkElement = screen.getByText(/Test_Topic/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Opens up Dialog modal with related topics on clicking the topic", () => {
+  const { getByText } = render(
+    <ApolloProvider client={client}>
+      <Topic
+        topic={{
+          name: "Test_Topic",
+          stargazerCount: 15,
+          relatedTopics: [
+            {
+              name: "Test_Other_Topic",
+              stargazerCount: 10,
+            },
+            {
+              name: "Test_Yet_Another_Topic",
+              stargazerCount: 10,
+            },
+          ],
+        }}
+      />
+    </ApolloProvider>
+  );
+  const topicListItem = getByText("Test_Topic");
+  fireEvent.click(topicListItem);
+  const relatedTopicItem = getByText("Test_Other_Topic");
+  expect(relatedTopicItem).toBeInTheDocument();
+});
